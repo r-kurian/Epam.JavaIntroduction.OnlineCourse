9b84573d2f649f5169b06d7e4c39d85738f38cb6
r-kurian <76216762+r-kurian@users.noreply.github.com>
2021-03-31 15:42:10 +0200

    new task is added

---
+++ src/Epam/Strings_and_basics_of_text_processing/Part3_3/Part3_3_Regex_Ex1_Var2.java
@@ -0,0 +1,122 @@
+package Epam.Strings_and_basics_of_text_processing.Part3_3;
+
+/*
+Создать приложение, разбирающее текст (хранится в строке) и позволяющее выполнять с текстом три различных операции:
+1) отсортировать абзацы по количеству предложений; +
+2) в каждом предложении отсортировать слова по длине;
+3) отсортировать лексемы в предложении по убыванию количества вхождений заданного символа,
+а в случае равенства – по алфавиту.
+
+
+Create an application that parses the text (stored as a string)
+and allows you to perform three different operations on the text:
+1) sort paragraphs by the number of sentences;
+2) sort words by a length in each sentence;
+3) sort lexemes in a sentence in descending order of the number of occurrences of a given character,
+and in case of equality, alphabetically.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Part3_3_Regex_Ex1_Var2 {
+
+
+    private static final String PARAGRAPH_SPLIT_REGEX = "(?m)(?!\\A)(?=\\s{4})";
+    private static final String SENTENCE_SPLIT_REGEX = "(?<=\\.|\\?)\\s(?=[A-Z])";
+
+    static String[] paragraphSplit(String myStringFromFile) {
+        return myStringFromFile.split(PARAGRAPH_SPLIT_REGEX);
+    }
+
+    static String[] sentenceSplit(String paragraphString) {
+        return paragraphString.split(SENTENCE_SPLIT_REGEX);
+    }
+
+    public static String[] paragraphSort(String[] paragraphArray) {
+        int[] lengthSentences = new int[paragraphArray.length];
+        String[] forParagraphSort = new String[paragraphArray.length];
+
+        for (int i = 0; i < paragraphArray.length; i++) {
+            String[] sentence = sentenceSplit(paragraphArray[i]);
+            lengthSentences[i] = sentence.length;
+        }
+
+        for (int i = 0; i < paragraphArray.length; i++) {
+            int count = Integer.MAX_VALUE;
+            int index = 0;
+            for (int j = 0; j < lengthSentences.length; j++) {
+                if (count > lengthSentences[j] && lengthSentences[j] != 0) {
+                    count = lengthSentences[j];
+                    index = j;
+                }
+            }
+            lengthSentences[index] = 0;
+            forParagraphSort[i] = paragraphArray[index];
+        }
+        return forParagraphSort;
+    }
+
+    public static List<String[]> wordSort(String[] sentence) {
+        List<String[]> listSentences = new ArrayList<>();
+
+        Comparator<String> comparator = (o1, o2) -> o1.length() - o2.length();
+
+        for (int i = 0; i < sentence.length; i++) {
+            listSentences.add(sentence[i].split("\\s"));
+        }
+
+        for (String[] s : listSentences) {
+            Arrays.sort(s, comparator);
+            System.out.print(Arrays.toString(s));
+        }
+        System.out.println();
+        return listSentences;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Enter your file path:"); // C:\Users\UserName\Desktop\java.txt
+
+        Scanner scannerForPath = new Scanner(System.in);
+        String myPathScanner = scannerForPath.nextLine();
+
+        Scanner scannerForFile = new Scanner(new File(myPathScanner));
+        String myStringFromFile = scannerForFile.useDelimiter("\\A").next();
+        scannerForFile.close();
+
+        String[] paragraphArray = paragraphSplit(myStringFromFile);
+        String paragraphString = paragraphArray.toString();
+        String[] paragraphSort = sentenceSplit(paragraphString);
+        String[] paragraphSortResult = paragraphSort(paragraphArray);
+        String[] sentence = sentenceSplit(paragraphArray[1]);
+
+
+        Scanner scannerForPrinting = new Scanner(System.in);
+
+        System.out.println("\n >>> Enter 1 to PRINT all PARAGRAPHS. Enter 0 to skip: ");
+        int printParagraphsArray = scannerForPrinting.nextInt();
+        if (printParagraphsArray == 1) {
+            System.out.println(Arrays.toString(paragraphArray));
+        }
+
+        System.out.println("\n >>> Enter 1 to SORT all PARAGRAPHS. Enter 0 to skip: ");
+        int printSortedParagraphs = scannerForPrinting.nextInt();
+        if (printSortedParagraphs == 1) {
+            System.out.println(Arrays.toString(paragraphSortResult));
+        }
+
+        System.out.println("\n >>> Enter 1 to SORT all SENTENCE in first paragraph. Enter 0 to skip: ");
+        int printSortedSentence = scannerForPrinting.nextInt();
+        if (printSortedSentence == 1) {
+            System.out.println(Arrays.toString(sentence));
+        }
+
+        System.out.println("\n >>> Enter 1 to SORT WORDS. Enter 0 to skip: ");
+        int printETWAS = scannerForPrinting.nextInt();
+        if (printETWAS == 1) {
+            System.out.println(wordSort(paragraphSortResult));
+        }
+    }
+}
